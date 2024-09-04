5e50253cd63351ed2e8f32321270cb618213f125
Ahtasham Halim Rifat <ahtashamhalim@gmail.com>
2024-09-04 20:44:49 +0600

    Making a Floyd's triangle using Java Program

---
+++ FloydTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n, row, col, count = 0;
+        System.out.print("Enter n:");
+        n = input.nextInt();
+        input.close();
+        for ( row = 1; row <= n; row++) {
+            for ( col = 1; col <= row; col++) {
+                System.out.print(++count+" ");
+            }
+            System.out.println();
+        }
+    }
+}
