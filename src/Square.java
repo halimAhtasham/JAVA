4945816304b30b680404407a906a95cda525c5a3
halimAhtasham <ahtashamhalim@gmail.com>
2024-09-08 22:53:47 +0600

    Simple Square shape area calculation with class

---
+++ src/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    public double side;
+
+    public Square(){
+        side  = 1;
+    }
+
+    public double area (){
+        double c = side * side;
+        return c;
+    }
+}
---
+++ src/SquareShape.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SquareShape {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Square shape = new Square();
+        System.out.print("Enter the side of the square: ");
+        shape.side = input.nextDouble();
+        double GivenArea = shape.area();
+        System.out.println("The area of the square is: "+GivenArea);
+        input.close();
+    }
+}
