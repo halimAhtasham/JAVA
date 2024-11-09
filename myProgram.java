f56437ea07c0138d4fa67ad3e132e8c93f675054
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-09 22:33:12 +0600

    nn

--- myProgram.java
+++ myProgram.java
@@ -1,22 +1,31 @@
 
 package sub.cse;
-
 import java.util.Scanner;
+import www.myclass.Calc;
+public class myprogram {
 
-import www.sub.Calculator;
-
-public class myProgram {
-
-    
+  
     public static void main(String[] args) {
-       
-       Scanner s = new Scanner(System.in);
-       Calculator c = new Calculator();
-       System.out.println("Enter two value");
-       c.a = s.nextDouble();
-       c.b = s.nextDouble();
-       double x = c.divi();
-       System.out.println(x);
-       
-    } 
-}
+        
+        Scanner input = new Scanner(System.in);
+        Calc c = new Calc();
+        
+//        c.n = 15;
+//        c.r = 5;
+         System.out.print("Enter value for n: ");
+        c.n = input.nextInt();
+        
+        System.out.print("Enter value for r: ");
+        c.r = input.nextInt();
+        
+        
+        long outputnCr = c.ncr();
+        long outputnPr = c.npr();
+        long outputfact = c.fact(12);
+        System.out.println("NCR = "+outputnCr);
+        System.out.println("NPR = "+outputnPr);
+        System.out.println("Factorial = "+outputfact);
+      
+    } // end of main method
+    
+} // end of main class
