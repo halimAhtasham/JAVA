8657310e800792799f9f8b23e3a4a2f4b9f3d990
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-09 22:32:07 +0600

    new

---
+++ src/Insomnia.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Insomnia {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k, l, m, n, d;
+        k = input.nextInt();
+        l = input.nextInt();
+        m = input.nextInt();
+        n = input.nextInt();
+        d = input.nextInt();
+        int count = 0;
+        for (int i = 1; i <= d; i++) {
+            if( i % k == 0 || i % m == 0 || i % n == 0 || i % l == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        input.close();
+    }
+}
