6358daa4e1546b5f41b5f1e1ee246498ebf93fcf
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-15 23:21:22 +0600

    n

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,12 +1,12 @@
 public class Exercise {
     public static void main(String[] args) {
         StringBuffer s = new StringBuffer("HelloWorld");
-        System.out.println(s.replace(0, 4, " Java"));
+        System.out.println(s.replace(0, 4, "Java"));
         System.out.println(s.append(" New"));
         s.insert(10, "This is");
         System.out.println(s);
         char sb = s.charAt(15);
         System.out.println(sb);
-        System.err.println("Hello");
+        System.err.println("Hello"); //hello
     }
 }
