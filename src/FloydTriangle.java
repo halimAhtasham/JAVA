7490b9d24ca0ba78019a29f3b33722741c8e0630
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-23 22:53:08 +0600

    n

--- src/FloydTriangle.java
+++ src/FloydTriangle.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class FloydTriangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int n, row, col, count = 0;
         System.out.print("Enter n:");
         n = input.nextInt();
