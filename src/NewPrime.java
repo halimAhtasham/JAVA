39b939336e80c1e5a1ce617e5342b8c59185a7c7
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-26 00:10:26 +0600

    n

---
+++ src/NewPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NewPrime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int flag = 0, count = 0;
+
+        for (int i = x; i <= y; i++) {
+            if (i < 2) {
+                continue;
+            }
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag++;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.print(i + " ");
+                count++;
+            }
+            flag = 0;
+        }
+        System.out.println("\nTotal Prime Number between " + x + " and "+ y + ": "+count);
+        input.close();
+    }
+}
