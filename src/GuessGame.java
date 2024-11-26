b162f03deebe8e65209613192ab2cbc8890eb283
halimAhtasham <ahtashamhalim@gmail.com>
2024-11-26 20:35:23 +0600

    c

---
+++ src/GuessGame.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int y = (int)(Math.random()*1000);
+        int count = 0;
+        System.out.println("Guess the number: ");
+        while (true) {
+            int x = input.nextInt();
+            count++;
+            if (x < y) {
+                System.out.println("Your guess is lower.");
+            }
+            else if (x > y) {
+                System.out.println("Your guess is higher.");
+            }
+
+            else{
+                System.out.println("Your guess is correct.");
+                break;
+            }
+        }
+        System.out.println("You have tried "+ count+ " times.");
+        input.close();
+    }
+}
