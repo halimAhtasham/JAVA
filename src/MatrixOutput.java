92cc2ca02394e51980781734dbbd1604dba1d811
halimAhtasham <ahtashamhalim@gmail.com>
2024-12-05 22:19:07 +0600

    c

--- src/MatrixOutput.java
+++ src/MatrixOutput.java
@@ -3,7 +3,6 @@
 public class MatrixOutput {
     public static void main(String[] args) {
         // Scanner input = new Scanner(System.in);
-
         int[][] A = new int[4][4];
         int count= 0 ;
         for (int row = 0; row < 4; row++) {
