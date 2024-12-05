0ad1309ebc0a6935a20ecf13e87033aec0e09994
halimAhtasham <ahtashamhalim@gmail.com>
2024-12-05 22:19:35 +0600

    c

--- src/MatrixOutput.java
+++ src/MatrixOutput.java
@@ -11,7 +11,7 @@ public class MatrixOutput {
                 count++;
             }
         }
-        
+
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col <= row; col++) {
                 System.out.print(A[row][col]+"\t");
