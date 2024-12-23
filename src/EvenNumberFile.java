2414d38bd16c76ba76ced1f4ebcc0bc163752d0f
halimAhtasham <ahtashamhalim@gmail.com>
2024-12-23 18:36:36 +0600

    c

--- src/EvenNumberFile.java
+++ src/EvenNumberFile.java
@@ -10,6 +10,7 @@ public class EvenNumberFile {
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
                 msg += i + " ";
+                
             }
         }
         try{
