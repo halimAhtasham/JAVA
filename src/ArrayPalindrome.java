c328368fae12554debcb620dcc034783b0886396
halimAhtasham <ahtashamhalim@gmail.com>
2024-12-26 11:01:05 +0600

    ArrayPalindrome class to check if an array is a palindrome

---
+++ src/ArrayPalindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArrayPalindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [] a = new int[n];
+        for (int i = 0; i < n; i++){
+            a[i] = in.nextInt();
+        }
+        in.close();
+        int i, j, flag = 0;
+        for(i=0, j = n-1; i <= j; i++, j-- ){
+            if(a[i] != a[j]){
+                flag++;
+                break;
+            }
+        }
+        if(flag == 0){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+        
+    }
+}
