919d88849a04e6495e98d8aa481fbfd604fdca68
halimAhtasham <ahtashamhalim@gmail.com>
2025-01-08 20:52:10 +0600

    c

--- src/StreamNode.java
+++ src/StreamNode.java
@@ -2,7 +2,7 @@ public class StreamNode {
     public static void main(String[] args) {
         for (int i = 0; i <= 8; i++) {
             System.out.println("StreamNode");
-            
+        
         }
     }
 }
