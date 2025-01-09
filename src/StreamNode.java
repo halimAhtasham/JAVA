f1b73f41065d360d8f4b5e68632df412a7f7db63
halimAhtasham <ahtashamhalim@gmail.com>
2025-01-09 21:56:01 +0600

    n

--- src/StreamNode.java
+++ src/StreamNode.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class StreamNode {
     public static void main(String[] args) {
-        for (int i = 0; i <= 8; i++) {
+        for (int i = 0; i <= 1; i++) {
             System.out.println("StreamNode");
         }
+        int [] a = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(a));
     }
 }
