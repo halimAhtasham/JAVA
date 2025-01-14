e3f31d906bee4fcdc5ce5bb9badc5cc6dd5e557b
halimAhtasham <ahtashamhalim@gmail.com>
2025-01-14 23:54:45 +0600

    Refactor GroupStudent class for improved clarity and comments

--- src/GroupStudent.java
+++ src/GroupStudent.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 public class GroupStudent {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter total number of students: ");
+        System.out.println("Enter total number of students: "); // getting the size
         int total = in.nextInt();
 
         System.out.println("Enter the roll of the students: ");
-        ArrayList<Integer> students = new ArrayList<Integer>();
+        ArrayList<Integer> students = new ArrayList<Integer>(); // added Rolls in the ArrayList
         for (int i = 0; i < total; i++) {
             students.add(in.nextInt());
         }
 
-        Collections.shuffle(students);
-        System.out.println("How many groups do you want to make?");
-        int groups = in.nextInt(); // total 
+        Collections.shuffle(students);// shuffling students in the groups
 
+        System.out.println("How many groups do you want to make?");
+        int groups = in.nextInt(); // total groups
 
         System.out.println("What is your group size?");
         int groupSize = in.nextInt(); // no of students in each group
         int remaining = total % groups;
         int index = 0;
 
-
-        for (int i = 0; i < groups; i++) {  // shuffle in the groups
+        for (int i = 0; i < groups; i++) {  // Storing Rolls in the groups 
             System.out.println("Group " + (i + 1) + ": ");
             for (int j = 0; j < groupSize; j++) {
                 System.out.println(students.get(index++) + " ");
